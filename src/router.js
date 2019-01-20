2621935208ebb5e16777d24dee48ff94d31d2009
Renato Böhler <renato.bohler@gmail.com>
2019-01-20 13:35:57 -0200

    feat(changelog): implements changelog route

--- src/router.js
+++ src/router.js
@@ -14,11 +14,12 @@ export default new Router({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: () => import("./views/About.vue")
+    },
+    {
+      path: "/changelog",
+      name: "changelog",
+      component: () => import("./views/Changelog.vue")
     }
   ]
 });
---
+++ src/views/Changelog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Changelog</h1>
+    <Changelog />
+  </div>
+</template>
+
+<script>
+import Changelog from "../../CHANGELOG.md";
+
+export default {
+  name: "home",
+  components: {
+    Changelog
+  }
+};
+</script>
