accfed3cf93e2aa711148febf59a9a2d07a6d018
Renato Böhler <renato.bohler@gmail.com>
2019-01-20 00:46:11 -0200

    feat: project initialization

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>I'm still stuying VueJS to build this page. Please, come back later.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-style: italic;
+}
+</style>
