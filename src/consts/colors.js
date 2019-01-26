466048e6f1ed8a1d5906d244c60faa965d6bd9af
Renato Böhler <renato.bohler@gmail.com>
2019-01-26 11:34:30 -0200

    feat: adds gradient colors constants

---
+++ src/consts/colors.js
@@ -0,0 +1,68 @@
+export const gradient = [
+  {
+    name: "dawn",
+    primary: {
+      bright: "#9566e3",
+      dark: "#4a3371"
+    },
+    secondary: {
+      bright: "#a7f5ff",
+      dark: "#52797e"
+    }
+  },
+  {
+    name: "morn",
+    primary: {
+      bright: "#96dcf7",
+      dark: "#4a6d7a"
+    },
+    secondary: {
+      bright: "#63ebbd",
+      dark: "#31745e"
+    }
+  },
+  {
+    name: "noon",
+    primary: {
+      bright: "#f2f572",
+      dark: "#787939"
+    },
+    secondary: {
+      bright: "#ff8484",
+      dark: "#7e4141"
+    }
+  },
+  {
+    name: "after",
+    primary: {
+      bright: "#fbdfac",
+      dark: "#7c6e55"
+    },
+    secondary: {
+      bright: "#ec6262",
+      dark: "#753131"
+    }
+  },
+  {
+    name: "dusk",
+    primary: {
+      bright: "#878ee4",
+      dark: "#434671"
+    },
+    secondary: {
+      bright: "#feb8cb",
+      dark: "#7e5b65"
+    }
+  },
+  {
+    name: "night",
+    primary: {
+      bright: "#0249b5",
+      dark: "#01245a"
+    },
+    secondary: {
+      bright: "#878ee4",
+      dark: "#424671"
+    }
+  }
+];
