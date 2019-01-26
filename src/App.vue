dbab722509363b7fd2a81c31c33ca0f2c1f6420e
Renato Böhler <renato.bohler@gmail.com>
2019-01-26 11:34:50 -0200

    feat(App): adds the Header component

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,185 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">{{ $t("route.home") }}</router-link> |
-      <router-link to="/about">{{ $t("route.about") }}</router-link> |
-      <router-link to="/changelog">{{ $t("route.changelog") }}</router-link>
-    </div>
-    <div id="lang">
-      <button
-        v-for="language in languages"
-        :key="language.title"
-        @click="changeLocale(language.locale)"
-      >
-        <flag :iso="language.flag" v-bind:squared="false" />
-      </button>
-    </div>
+  <div id="app" :style="cssVariables">
+    <Header />
     <router-view />
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
 import i18n, { languages } from "@/plugins/i18n.js";
+import { gradient } from "@/consts/colors";
+
+const randomColor = gradient[Math.floor(Math.random() * gradient.length)];
 
 export default {
+  components: {
+    Header
+  },
   data: () => ({
     languages
   }),
   methods: {
     changeLocale(locale) {
-      console.log(locale);
       i18n.locale = locale;
     }
+  },
+  computed: {
+    cssVariables: () => ({
+      // Menu
+      "--menu-shrinked-height": "50px",
+      "--menu-animation-duration": "0.5s",
+      // Gradient colors
+      "--gradient-primary-bright": randomColor.primary.bright,
+      "--gradient-primary-dark": randomColor.primary.dark,
+      "--gradient-secondary-bright": randomColor.secondary.bright,
+      "--gradient-secondary-dark": randomColor.secondary.dark
+    })
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Ubuntu", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding: 0;
+  margin: 0;
+  vertical-align: baseline;
+  list-style: none;
+  border: 0;
 }
 
-#nav {
-  padding: 20px;
+body {
+  background: #f4f5f7;
+  /* TODO: remove */
+  min-height: 2000px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+@keyframes gradient {
+  0% {
+    background-position: 50% 0%;
+  }
+  50% {
+    background-position: 50% 100%;
+  }
+  100% {
+    background-position: 50% 0%;
+  }
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+@-moz-keyframes gradient {
+  0% {
+    background-position: 50% 0%;
+  }
+  50% {
+    background-position: 50% 100%;
+  }
+  100% {
+    background-position: 50% 0%;
+  }
 }
 
-#lang {
-  display: flex;
-  justify-content: center;
-  padding: 0 0 10px 0;
+@-webkit-keyframes gradient {
+  0% {
+    background-position: 50% 0%;
+  }
+  50% {
+    background-position: 50% 100%;
+  }
+  100% {
+    background-position: 50% 0%;
+  }
 }
 
-button {
-  border: 1px solid #42b983;
-  background: #efefef;
-  padding: 5px;
-  margin: 5px;
+@keyframes gradient-horizontal {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+@-moz-keyframes gradient-horizontal {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+@-webkit-keyframes gradient-horizontal {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+@keyframes shrink {
+  0% {
+    height: 100%;
+  }
+  100% {
+    height: var(--menu-shrinked-height);
+  }
+}
+
+@-moz-keyframes shrink {
+  0% {
+    height: 100%;
+  }
+  100% {
+    height: var(--menu-shrinked-height);
+  }
+}
+
+@-webkit-keyframes shrink {
+  0% {
+    height: 100%;
+  }
+  100% {
+    height: var(--menu-shrinked-height);
+  }
+}
+
+@keyframes expand {
+  0% {
+    height: var(--menu-shrinked-height);
+  }
+  100% {
+    height: 100%;
+  }
+}
+
+@-moz-keyframes expand {
+  0% {
+    height: var(--menu-shrinked-height);
+  }
+  100% {
+    height: 100%;
+  }
+}
+
+@-webkit-keyframes expand {
+  0% {
+    height: var(--menu-shrinked-height);
+  }
+  100% {
+    height: 100%;
+  }
 }
 </style>
