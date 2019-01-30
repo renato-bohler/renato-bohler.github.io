fc70c9d3e67d6c91dc69b3297a3de9dfc0e988bb
Renato Böhler <renato.bohler@gmail.com>
2019-01-29 22:13:54 -0200

    feat(main): adds scrollto plugin to the vue instance

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import FlagIcon from "vue-flag-icon";
+import ScrollTo from "vue-scrollto";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "@/plugins/i18n";
 
 Vue.use(FlagIcon);
+Vue.use(ScrollTo, {
+  offset: -100
+});
 
 Vue.config.productionTip = false;
 
