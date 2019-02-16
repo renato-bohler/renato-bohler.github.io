2adb6fa7bdf1c27a9ed09ce073f8f077660c1b74
Renato Böhler <renato.bohler@gmail.com>
2019-02-16 14:49:15 -0200

    feat(App): removes stub divider divs

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,9 @@
     <Header :fullscreen="fullscreen" />
     <div class="app-content-container">
       <About />
-      <div
-        style="height: 20px"
-        v-for="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
-        :key="i"
-      />
       <Skills />
-      <div
-        style="height: 20px"
-        v-for="i in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]"
-        :key="i"
-      />
       <Projects />
-      <div
-        style="height: 20px"
-        v-for="i in [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]"
-        :key="i"
-      />
       <Professional />
-      <div
-        style="height: 20px"
-        v-for="i in [31, 32, 33, 34, 35, 36, 37, 38, 39, 40]"
-        :key="i"
-      />
     </div>
     <Footer />
   </div>
