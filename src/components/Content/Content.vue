543788b3ff90be7c762677ee70c30d9a053a9e8d
Renato Böhler <renato.bohler@gmail.com>
2019-02-16 14:50:19 -0200

    feat(Content): initial implementation

---
+++ src/components/Content/Content.vue
@@ -0,0 +1,19 @@
+<template>
+  <ContentContainer>
+    <ContentFrame :title="title" :lastEditedAt="lastEditedAt">
+      <slot />
+    </ContentFrame>
+    <ContentDivider />
+  </ContentContainer>
+</template>
+
+<script>
+import ContentContainer from "./ContentContainer";
+import ContentFrame from "./ContentFrame";
+import ContentDivider from "./ContentDivider";
+
+export default {
+  components: { ContentContainer, ContentFrame, ContentDivider },
+  props: ["title", "lastEditedAt"]
+};
+</script>
---
+++ src/components/Content/ContentFrame.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <div class="title">{{ title }}</div>
+    <div class="frame">
+      <div class="content">
+        <slot />
+      </div>
+      <div class="footer">
+        <icon name="regular/clock" class="icon" scale="0.8" />
+        {{ $t("content.lastEdited") }}
+        {{ relativeEditTime }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "lastEditedAt"],
+  computed: {
+    relativeEditTime() {
+      return this.$moment(this.lastEditedAt).fromNow();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.frame {
+  display: flex;
+  flex-direction: column;
+  background: #f3f3f3;
+  border-top: 3px groove var(--theme-border);
+  width: 900px;
+  min-height: 200px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+}
+
+.title {
+  font-family: "Major Mono Display", monospace !important;
+  font-size: 3em;
+  font-weight: bold;
+  margin-bottom: -0.15em;
+  color: var(--theme-primary-dark);
+  z-index: 2;
+}
+
+.content {
+  flex-grow: 1;
+  padding: 16px;
+  font-size: 0.9em;
+}
+
+.content > p {
+  text-indent: 20px;
+}
+
+.footer {
+  display: flex;
+  justify-content: flex-end;
+  padding: 8px;
+  background: #e0e0e0;
+  border-top: 1px dashed #888888;
+  border-bottom: 1px solid #cacaca;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  font-size: 0.7em;
+  font-style: italic;
+  color: #333;
+}
+
+.footer > .icon {
+  margin-right: 4px;
+}
+
+@media screen and (max-width: 900px) {
+  .frame {
+    width: 100%;
+  }
+
+  .content {
+    padding: 8px;
+  }
+
+  .footer {
+    padding: 4px;
+  }
+}
+</style>
