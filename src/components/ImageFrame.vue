c398e5a905b3310f2dccb3de390f93d6086b2e0b
Renato Böhler <renato.bohler@gmail.com>
2019-02-18 20:18:19 -0300

    feat(ImageFrame): implements image component

---
+++ src/components/ImageFrame.vue
@@ -0,0 +1,48 @@
+<template>
+  <img
+    :src="require(`@/images/${this.src}`)"
+    :class="[format, border && 'with-border']"
+    :style="cssVariables"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String
+    },
+    format: {
+      type: String,
+      default: "circle"
+    },
+    size: {
+      type: Number,
+      default: 100
+    },
+    border: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    cssVariables() {
+      return {
+        "--image-frame-size": `${this.size}px`
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.circle {
+  border-radius: 100%;
+  width: var(--image-frame-size);
+  height: var(--image-frame-size);
+}
+
+.with-border {
+  border: 2px solid var(--theme-border);
+}
+</style>
