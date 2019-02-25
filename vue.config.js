1edd75d2b151fcf32a7c8c6f5ffc2f5ffbabc11e
Renato Böhler <renato.bohler@gmail.com>
2019-02-24 21:00:33 -0300

    perf: reduces bundle size by loading only used momentjs locales

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,17 @@
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
+
 module.exports = {
   configureWebpack: config => {
+    // Markdown loader
     config.module.rules.push({
       test: /\.md$/,
       loader: "vue-loader!vue-md-loader"
     });
+    // MomentJS locale loader
+    config.plugins.push(
+      new MomentLocalesPlugin({
+        localesToKeep: ["pt-br"]
+      })
+    );
   }
 };
