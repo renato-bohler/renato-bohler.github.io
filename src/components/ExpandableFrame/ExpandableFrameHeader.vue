e91f203df8b6faac635902cfd3175b005148ea8e
Renato Böhler <renato.bohler@gmail.com>
2019-03-07 22:07:13 -0300

    refactor(ExpandableFrame): splits into multiple components

---
+++ src/components/ExpandableFrame/ExpandableFrameHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="header" @click.stop="toggle">
+    <ImageFrame :src="image" :size="30" :border="true" />
+    <h3 class="title">{{ title }}</h3>
+    <ExpandableFrameMoreInfo :moreInfo="moreInfo" />
+    <span :class="['chevron', isOpen]">></span>
+  </div>
+</template>
+
+<script>
+import ExpandableFrameMoreInfo from "./ExpandableFrameMoreInfo";
+import ImageFrame from "@/components/ImageFrame";
+
+export default {
+  props: ["isOpen", "title", "image", "toggle", "moreInfo"],
+  components: { ExpandableFrameMoreInfo, ImageFrame }
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  -webkit-touch-callout: none;
+  -webkit-tap-highlight-color: transparent;
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+.title {
+  flex-grow: 1;
+  font-weight: normal;
+  padding-left: 16px;
+  margin: 0;
+}
+
+.chevron {
+  display: inline-block;
+  transition: transform 0.5s ease;
+}
+
+.chevron.open {
+  transform: rotate(-90deg);
+}
+
+.chevron.close {
+  transform: rotate(90deg);
+}
+</style>
---
+++ src/components/ExpandableFrame/index.js
@@ -0,0 +1,1 @@
+export { default } from "./ExpandableFrame.vue";
