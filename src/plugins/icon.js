2b1725f7b0550ee1b1a37bf3cdec2ef3c506a3c3
Renato Böhler <renato.bohler@gmail.com>
2019-03-07 22:10:10 -0300

    feat(icon): adds icons used on Skills section

--- src/plugins/icon.js
+++ src/plugins/icon.js
@@ -2,5 +2,15 @@ import Icon from "vue-awesome/components/Icon";
 
 // All used icons
 import "vue-awesome/icons/regular/clock";
+import "vue-awesome/icons/level-up-alt";
+import "vue-awesome/icons/level-down-alt";
+import "vue-awesome/icons/minus";
+import "vue-awesome/icons/dice-one";
+import "vue-awesome/icons/dice-two";
+import "vue-awesome/icons/dice-three";
+import "vue-awesome/icons/dice-four";
+import "vue-awesome/icons/dice-five";
+import "vue-awesome/icons/dice-six";
+import "vue-awesome/icons/book-reader";
 
 export default Icon;
