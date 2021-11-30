7922b693318d6bac068646d10626e6dcab335c85
Renato Böhler <renato.bohler@gmail.com>
2021-11-30 16:37:09 -0300

    feat: implements skills header and teasers

---
+++ components/sections/Skills/SkillCard/SkillCard.tsx
@@ -0,0 +1,187 @@
+import { useState } from 'react';
+
+import classNames from 'classnames';
+import { useInView } from 'react-intersection-observer';
+import Tilt from 'react-parallax-tilt';
+import { Button } from 'reakit/Button';
+
+import Icon from '~/components/Icon/Icon';
+
+import styles from './SkillCard.module.css';
+
+type Props = {
+  name: string;
+  description: React.ReactElement;
+  teaser?: React.ReactElement;
+  icon: React.ReactNode;
+  favorite: boolean;
+  color: string;
+  textColor: string;
+  usageLevel: -3 | -2 | -1 | 1 | 2 | 3;
+  yearsExperience?: number;
+  studying: boolean;
+};
+
+const WAVE_PATHS = [
+  'M0,32L48,42.7C96,53,192,75,288,96C384,117,480,139,576,133.3C672,128,768,96,864,69.3C960,43,1056,21,1152,16C1248,11,1344,21,1392,26.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+  'M0,192L48,213.3C96,235,192,277,288,261.3C384,245,480,171,576,149.3C672,128,768,160,864,176C960,192,1056,192,1152,165.3C1248,139,1344,85,1392,58.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+  'M0,256L48,234.7C96,213,192,171,288,128C384,85,480,43,576,53.3C672,64,768,128,864,160C960,192,1056,192,1152,176C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+  'M0,96L48,106.7C96,117,192,139,288,144C384,149,480,139,576,149.3C672,160,768,192,864,218.7C960,245,1056,267,1152,250.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+  'M0,288L48,293.3C96,299,192,309,288,298.7C384,288,480,256,576,208C672,160,768,96,864,106.7C960,117,1056,203,1152,245.3C1248,288,1344,288,1392,288L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+  'M0,224L48,234.7C96,245,192,267,288,256C384,245,480,203,576,192C672,181,768,203,864,213.3C960,224,1056,224,1152,218.7C1248,213,1344,203,1392,197.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+  'M0,0L48,21.3C96,43,192,85,288,122.7C384,160,480,192,576,208C672,224,768,224,864,192C960,160,1056,96,1152,90.7C1248,85,1344,139,1392,165.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+  'M0,288L48,272C96,256,192,224,288,186.7C384,149,480,107,576,96C672,85,768,107,864,144C960,181,1056,235,1152,266.7C1248,299,1344,309,1392,314.7L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+  'M0,128L48,128C96,128,192,128,288,117.3C384,107,480,85,576,85.3C672,85,768,107,864,117.3C960,128,1056,128,1152,128C1248,128,1344,128,1392,128L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+  'M0,288L48,266.7C96,245,192,203,288,181.3C384,160,480,160,576,165.3C672,171,768,181,864,176C960,171,1056,149,1152,138.7C1248,128,1344,128,1392,128L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
+];
+
+const USAGE = {
+  [-3]: {
+    icon: 'chevron-triple-down',
+    description: "I haven't used this in the last five years",
+  },
+  [-2]: {
+    icon: 'chevron-double-down',
+    description: "I haven't used this in the last two years",
+  },
+  [-1]: {
+    icon: 'chevron-down',
+    description: "I haven't used this in the last year",
+  },
+  1: {
+    icon: 'chevron-up',
+    description: "I've used this it the last year",
+  },
+  2: {
+    icon: 'chevron-double-up',
+    description: "I've been using this recently",
+  },
+  3: {
+    icon: 'chevron-triple-up',
+    description: "I've been using this everyday",
+  },
+};
+
+const SkillCard: React.VFC<Props> = ({
+  name,
+  teaser,
+  icon,
+  favorite,
+  color,
+  textColor,
+  usageLevel,
+  yearsExperience,
+  studying,
+}) => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+
+  const [wavePath] = useState(
+    WAVE_PATHS[Math.floor(Math.random() * WAVE_PATHS.length)],
+  );
+
+  const usage = USAGE[usageLevel];
+
+  // TODO: dialogs
+  // TODO: adjust colors for contrast (a11y) - lighthouse
+
+  return (
+    <div
+      ref={ref}
+      className={classNames(styles.card, {
+        [styles.large]: favorite,
+        [styles.hidden]: !inView,
+      })}
+      style={{
+        background: color,
+      }}
+    >
+      <Tilt
+        className={styles.tilt}
+        tiltMaxAngleX={favorite ? 5 : 10}
+        tiltMaxAngleY={favorite ? 5 : 10}
+      >
+        <Button
+          className={styles.button}
+          style={{ color: textColor }}
+        >
+          <div className={styles.image}>{icon}</div>
+
+          <span className={styles.name}>{name}</span>
+
+          {favorite && teaser && (
+            <div className={styles.teaser}>{teaser}</div>
+          )}
+
+          <div className={styles.info}>
+            <svg
+              viewBox="0 0 1440 320"
+              className={styles['wave-divider']}
+            >
+              <path
+                fill="var(--theme-background-opaque)"
+                d={wavePath}
+              ></path>
+            </svg>
+            <div className={styles['content-wrapper']}>
+              {yearsExperience && (
+                <>
+                  <div className={styles.content}>
+                    <span
+                      className={styles['content-title']}
+                      aria-label="experience"
+                    >
+                      {favorite ? 'experience' : 'exp.'}
+                    </span>
+                    <span
+                      aria-label={`${yearsExperience} year${
+                        yearsExperience > 1 ? 's' : ''
+                      }`}
+                    >
+                      {yearsExperience} yr
+                      {yearsExperience > 1 ? 's' : ''}
+                    </span>
+                  </div>
+
+                  <span className={styles.divider}></span>
+                </>
+              )}
+              <div className={styles.content}>
+                <span
+                  className={styles['content-title']}
+                  aria-label="usage"
+                >
+                  {favorite ? 'usage' : 'usg.'}
+                </span>
+                <span title={usage.description}>
+                  <Icon set="mdi" name={usage.icon} />
+                </span>
+              </div>
+              {studying && (
+                <>
+                  <span className={styles.divider}></span>
+
+                  <div className={styles.content}>
+                    <span
+                      className={styles['content-title']}
+                      aria-label="studying"
+                    >
+                      {favorite ? 'studying' : 'stu.'}
+                    </span>
+                    <span title="I've been studying this recently">
+                      <Icon set="mdi" name="book-open-page-variant" />
+                    </span>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </Button>
+      </Tilt>
+    </div>
+  );
+};
+
+export default SkillCard;
---
+++ components/sections/Skills/Skills.tsx
@@ -0,0 +1,86 @@
+import { RefObject, useEffect, useState } from 'react';
+
+import { useInView } from 'react-intersection-observer';
+import { Tabbable } from 'reakit/Tabbable';
+
+import SkillCard from './SkillCard/SkillCard';
+import SkillRadar from './SkillRadar/SkillRadar';
+import skills from './skills.const';
+import styles from './Skills.module.css';
+
+type Props = {
+  firstRef: RefObject<HTMLHeadingElement>;
+};
+
+const Skills: React.VFC<Props> = ({ firstRef }) => {
+  const [header, setHeader] = useState('');
+
+  const [ref1, inView1] = useInView({ threshold: 1 });
+  const [ref2, inView2] = useInView({ threshold: 1 });
+  const [ref3, inView3] = useInView({ threshold: 1 });
+  const [ref4, inView4] = useInView({ threshold: 1 });
+  const [ref5, inView5] = useInView({ threshold: 1 });
+  const [ref6, inView6] = useInView({
+    threshold: 1,
+    rootMargin: '100% 0% 0% 0%',
+  });
+  const refs = [ref1, ref2, ref3, ref4, ref5, ref6];
+
+  useEffect(() => {
+    if (inView6) return setHeader('skills');
+    if (inView5) return setHeader('skill');
+    if (inView4) return setHeader('skil');
+    if (inView3) return setHeader('ski');
+    if (inView2) return setHeader('sk');
+    if (inView1) return setHeader('s');
+    setHeader('');
+  }, [inView1, inView2, inView3, inView4, inView5, inView6]);
+
+  return (
+    <section className={styles.section}>
+      <div className={styles.scroll}>
+        <Tabbable
+          as="h2"
+          className={styles.title}
+          aria-label="Skills"
+          ref={firstRef}
+        >
+          {header}
+        </Tabbable>
+
+        {refs.map((ref, index) => (
+          <div
+            key={index}
+            ref={ref}
+            className={styles.trigger}
+            style={{
+              top: `${40 + index * 10}%`,
+            }}
+          ></div>
+        ))}
+      </div>
+
+      <SkillRadar />
+
+      <div className={styles.grid}>
+        {skills.map((skill) => (
+          <SkillCard
+            key={skill.name}
+            name={skill.name}
+            icon={skill?.icon?.()}
+            favorite={skill.favorite}
+            color={skill.color}
+            textColor={skill.textColor}
+            usageLevel={skill.usageLevel}
+            yearsExperience={skill.yearsExperience}
+            studying={skill.studying}
+            description={skill.description}
+            teaser={skill.teaser}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
