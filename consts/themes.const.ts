2c54d180d47be91aa7181edab08a4fcd2809dd81
Renato Böhler <renato.bohler@gmail.com>
2022-10-06 11:36:03 -0300

    fix(theme): fixes list-style-image on dark mode

--- consts/themes.const.ts
+++ consts/themes.const.ts
@@ -1,3 +1,9 @@
+const listStyleImage = {
+  light:
+    'url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2220%22%20height%3D%2220%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22black%22%20stroke-width%3D%222%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3Cline%20x1%3D%225%22%20y1%3D%2212%22%20x2%3D%2219%22%20y2%3D%2212%22%3E%3C%2Fline%3E%3Cpolyline%20points%3D%2212%205%2019%2012%2012%2019%22%3E%3C%2Fpolyline%3E%3C%2Fsvg%3E")',
+  dark: 'url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2220%22%20height%3D%2220%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22white%22%20stroke-width%3D%222%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3Cline%20x1%3D%225%22%20y1%3D%2212%22%20x2%3D%2219%22%20y2%3D%2212%22%3E%3C%2Fline%3E%3Cpolyline%20points%3D%2212%205%2019%2012%2012%2019%22%3E%3C%2Fpolyline%3E%3C%2Fsvg%3E")',
+};
+
 const light = {
   'is-contrast': '0',
   'is-dark': '0',
@@ -16,6 +22,7 @@ const light = {
   'scrollbar-track-color': '#838383',
   'accent-color': 'auto',
   'chat-background': '#e9e9e9',
+  'list-style-image': listStyleImage.light,
 };
 
 const dark = {
@@ -36,6 +43,7 @@ const dark = {
   'scrollbar-track-color': '#424242',
   'accent-color': 'auto',
   'chat-background': '#1f242a',
+  'list-style-image': listStyleImage.dark,
 };
 
 export const contrast = {
@@ -66,6 +74,7 @@ export const contrast = {
     'secondary-dark': '#282828',
     'highlight-accent-rgb': '255, 255, 255',
     'highlight-rgb': '0, 0, 0',
+    'list-style-image': listStyleImage.light,
   },
   dark: {
     'is-contrast': '1',
@@ -93,6 +102,7 @@ export const contrast = {
     'secondary-dark': '#b3b3b3',
     'highlight-rgb': '255, 255, 255',
     'highlight-accent-rgb': '0, 0, 0',
+    'list-style-image': listStyleImage.dark,
   },
 };
 
