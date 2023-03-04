6a5849103e61d155aa687a45cc18f6e227f032f3
Renato Böhler <renato.bohler@gmail.com>
2023-03-04 17:42:20 -0300

    feat(ExperienceCard): implements revealing animation

--- components/sections/Experience/ExperienceCard/ExperienceCard.tsx
+++ components/sections/Experience/ExperienceCard/ExperienceCard.tsx
@@ -1,3 +1,5 @@
+import { useInView } from 'react-intersection-observer';
+
 import styles from './ExperienceCard.module.css';
 
 type Props = {
@@ -5,12 +7,19 @@ type Props = {
   year: number;
 };
 
-const ExperienceCard: React.FC<Props> = ({ children, year }) => (
-  <li className={styles.card}>
-    <div className={styles.dot} />
-    <div className={styles.year}>{year}</div>
-    <div className={styles.text}>{children}</div>
-  </li>
-);
+const ExperienceCard: React.FC<Props> = ({ children, year }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: '-50px',
+  });
+
+  return (
+    <li className={styles.card} data-revealed={inView}>
+      <div className={styles.dot} ref={ref} />
+      <div className={styles.year}>{year}</div>
+      <div className={styles.text}>{children}</div>
+    </li>
+  );
+};
 
 export default ExperienceCard;
