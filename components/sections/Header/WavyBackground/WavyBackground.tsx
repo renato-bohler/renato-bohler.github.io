dcf1927dbeb1e27476f3692cfa7e969362c347d0
Renato Böhler <renato.bohler@gmail.com>
2023-03-07 13:37:19 -0300

    refactor(WavyBackground): removes duplicate path declaration

--- components/sections/Header/WavyBackground/WavyBackground.tsx
+++ components/sections/Header/WavyBackground/WavyBackground.tsx
@@ -9,6 +9,10 @@ const WavyBackground: React.FC = () => {
     <div className={styles.waveBackground} aria-hidden>
       <svg>
         <defs>
+          <path
+            id="wave_path"
+            d="M 0 47.8 C 50.432 47.8 90.43 0 139.988 0 C 189.548 0 229.545 47.8 279.977 47.8 L 279.977 48.938 C 229.545 48.938 189.548 1.138 139.988 1.138 C 90.43 1.138 50.432 48.938 0 48.938 Z"
+          />
           {['primary', 'secondary'].map((type) => (
             <pattern
               key={type}
@@ -17,14 +21,14 @@ const WavyBackground: React.FC = () => {
               height="102"
               patternUnits="userSpaceOnUse"
             >
-              <path
+              <use
+                xlinkHref="#wave_path"
                 fill={
                   type === 'primary'
                     ? 'var(--theme-primary-bright)'
                     : 'var(--theme-secondary-dark)'
                 }
                 className={styles.wavePattern}
-                d="M 0 47.8 C 50.432 47.8 90.43 0 139.988 0 C 189.548 0 229.545 47.8 279.977 47.8 L 279.977 48.938 C 229.545 48.938 189.548 1.138 139.988 1.138 C 90.43 1.138 50.432 48.938 0 48.938 Z"
               />
             </pattern>
           ))}
