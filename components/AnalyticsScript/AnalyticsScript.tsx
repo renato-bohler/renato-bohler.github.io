a560b0b72dac4ed2e98e63d97154d8dabd54a8bb
Renato Böhler <renato.bohler@gmail.com>
2023-05-13 14:26:21 -0300

    chore: update Analytics to GA4

--- components/AnalyticsScript/AnalyticsScript.tsx
+++ components/AnalyticsScript/AnalyticsScript.tsx
@@ -3,16 +3,15 @@ import Script from 'next/script';
 const AnalyticsScript = () => (
   <>
     <Script
-      src="https://www.googletagmanager.com/gtag/js?id=UA-11798603-21"
+      src="https://www.googletagmanager.com/gtag/js?id=G-T5ZEK0KJZR"
       strategy="afterInteractive"
     />
     <Script id="google-analytics" strategy="afterInteractive">
       {[
         'window.dataLayer = window.dataLayer || [];',
-        'function gtag(){window.dataLayer.push(arguments);}',
+        'function gtag(){dataLayer.push(arguments);}',
         "gtag('js', new Date());",
-        '',
-        "gtag('config', 'UA-11798603-21');",
+        "gtag('config', 'G-T5ZEK0KJZR');",
       ].join('\n')}
     </Script>
   </>
