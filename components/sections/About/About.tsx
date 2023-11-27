9de737d690118a51a3f20723a88b4d3ace032408
Renato Böhler <renato.bohler@gmail.com>
2023-11-27 19:15:03 -0300

    chore: adds test change

--- components/sections/About/About.tsx
+++ components/sections/About/About.tsx
@@ -72,7 +72,7 @@ const About: React.FC = () => {
           ref={setAboutRefs}
         >
           <AnimatedChatMessage>
-            Hello there (test change number 3) 👋
+            Hello there (test change) 👋
           </AnimatedChatMessage>
         </div>
       </div>
