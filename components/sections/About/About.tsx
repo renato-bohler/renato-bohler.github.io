657989a3480c2b8764b1b28c831754b23893494d
Renato Böhler <renato.bohler@gmail.com>
2023-11-28 20:14:09 -0300

    chore: adds test change

--- components/sections/About/About.tsx
+++ components/sections/About/About.tsx
@@ -71,7 +71,9 @@ const About: React.FC = () => {
           )}
           ref={setAboutRefs}
         >
-          <AnimatedChatMessage>Hello there 👋</AnimatedChatMessage>
+          <AnimatedChatMessage>
+            Hello there 👋 (test)
+          </AnimatedChatMessage>
         </div>
       </div>
 
