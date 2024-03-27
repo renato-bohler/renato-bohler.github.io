ff9d66e013b5ffe9ece9980ec8f9a583dd005373
Renato Böhler <renato.bohler@gmail.com>
2024-03-26 22:05:46 -0300

    fix: removes reinitialization warning

--- hooks/useFullStory.ts
+++ hooks/useFullStory.ts
@@ -1,9 +1,10 @@
 import { useEffect } from 'react';
 
-import { init } from '@fullstory/browser';
+import { init, isInitialized } from '@fullstory/browser';
 
 const useFullStory = () => {
   useEffect(() => {
+    if (isInitialized()) return;
     init({ orgId: process.env.NEXT_PUBLIC_FULLSTORY_ORG_ID });
   }, []);
 };
