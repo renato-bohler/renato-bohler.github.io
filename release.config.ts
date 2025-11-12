92e9a9f52b3ee9fa0c5f13e4cb2e073a1daf43b4
Renato Böhler <renato.bohler@gmail.com>
2025-11-12 20:49:31 -0300

    ci: updates `semantic-release` configurations

---
+++ release.config.ts
@@ -0,0 +1,22 @@
+import { type GlobalConfig } from 'semantic-release';
+
+import packageJson from './package.json' with { type: 'json' };
+
+const config: GlobalConfig = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+  repositoryUrl: packageJson.repository.url,
+  tagFormat: 'v${version}',
+};
+
+export default config;
